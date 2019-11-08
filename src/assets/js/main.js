acb3dbab8b4b023d498023aa9e1c0ad5b22fbc5c
renanmurilo <renan@pravy.com.br>
2019-11-07 23:24:52 -0300

    home

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,6 +2,13 @@ $(function(){
     $('.carrousel-banner').slick({
         autoplay: true,
         dots: false,
-
     })
+});
+
+$(function(){
+    $('.carroussel-blog').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
 })
