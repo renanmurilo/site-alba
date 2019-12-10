c92c85f4819d36c76193f3c287968f3c98f4edf0
renanmurilo <renan@pravy.com.br>
2019-12-09 21:25:59 -0300

    finalizando projeto

--- src/assets/js/player.js
+++ src/assets/js/player.js
@@ -4,15 +4,23 @@ function playVid() {
 
   if(vid.paused){
     vid.play();
-    $( "#myVideo" ).click( "bg-play", function() {
-      $( ".btn-play" ).hide();
-      $(".title-video").hide();
-    });
   }else {
     vid.pause();
-    $( "#myVideo").click( function() {
-      $( ".btn-play" ).show();
-    });
   }
 } 
+
+function goPlay() {
+
+  if(vid.paused){
+    vid.play();
+    $('#veja3 .container').css('opacity', '0');
+    $('ul .numero').hide();
+    $('#myVideo').css('opacity', '1');
+  } else {
+    vid.pause();
+    $('#veja3 .container').css('opacity', '1');
+    $('ul .numero').show();
+    $('#myVideo').css('opacity', '0');
+  }
+}
 
